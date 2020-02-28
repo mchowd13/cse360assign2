0da7749f874211457e0ec424d22f301d8af82249
Tren Black <redwanc12@gmail.com>
2020-02-27 23:59:04 -0700

    added append function to simplelist class.

--- SimpleList.java
+++ SimpleList.java
@@ -1,9 +1,9 @@
 /**
  * @author      M Redwan Chowdhury
  * @class ID     386
- * @Assignment   Assigment #1
+ * @Assignment   Assigment #2
  * @Description  This file creates a list object using an array of integers.
- * It adds basic operations that you can expect from a list object.
+ * It adds basic operations that you can expect from a list object. Updated from assignment one.
  */
 
 public class SimpleList {
@@ -11,19 +11,49 @@ public class SimpleList {
     //It performs the add and remove operations while also maintaining an accurate count.
     private int[] list;
     private int count;
+    private int max_count;
 
     public SimpleList(){
         //Constructor. Initializes a new array with space for 10 ints.
         this.list = new int[10];
         this.count = 0;
+        this.max_count = 10;
     }
     public void add(int element){
         //Adds an element to the beginning of the list and shifts all other elements up.
         // Updates count.
+
+        if(this.count >= this.max_count-1){
+            this.increaseSize();
+        }
+
         this.shiftUp(0, this.count);
         this.list[0] = element;
         this.count++;
     }
+
+    public void increaseSize(){
+        //adds 50% more size to array
+        int new_count = this.max_count + (this.max_count/2);
+        int[] new_array = new int[new_count];
+        for(int index=0; index<this.count;index++){
+            new_array[index] = this.list[index];
+        }
+        this.max_count = new_count;
+        this.list = new_array;
+    }
+
+    public void decreaseSize(){
+        //removes 20% of the size of the array at the end.
+        int new_count = (int) (0.8 * this.max_count);
+        int[] new_array = new int[new_count];
+        for(int index=0; index<new_count;index++){
+            new_array[index] = this.list[index];
+        }
+        this.max_count = new_count;
+        this.list = new_array;
+    }
+
     public void remove(int element){
         //Searches for a element in the array. If found, it removes it. Updates count.
         for(int index=0; index<this.count; index++){
@@ -31,6 +61,10 @@ public class SimpleList {
                 this.list[index] = 0;
                 this.shiftDown(index, this.count);
                 this.count--;
+
+                if(this.count <= 0.75*this.max_count){
+                    this.decreaseSize();
+                }
                 break;
             }
         }
@@ -84,4 +118,18 @@ public class SimpleList {
 
     }
 
+    public void append(int element){
+        //adds element to end of array
+        if(this.count >= this.max_count-1){
+            this.increaseSize();
+        }
+        this.count += 1;
+        this.list[count] = element;
+    }
+    public int first(){
+        return(this.list[0]);
+    }
+    public int size(){
+        return(this.max_count);
+    }
 }
